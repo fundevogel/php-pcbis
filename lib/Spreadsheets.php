9e498f2aa4ba46a076ba3f604928060b89b6b3eb
Martin Folkers <hello@twobrain.io>
2022-02-23 12:37:43 +0100

    Make function 'csv2array' exclusive for use with CSV exported from 'pcbis.de'

--- lib/Spreadsheets.php
+++ lib/Spreadsheets.php
@@ -32,40 +32,6 @@ class Spreadsheets
      * Turns data from a single CSV file into a PHP array
      *
      * @param string $file - Source CSV file to read data from
-     * @param string $delimiter - Delimiting character
-     * @return array
-     */
-    public static function _csvOpen(string $file, string $delimiter = ';'): array
-    {
-        # Load file contents
-        $lines = file($file);
-
-        # Determine header row
-        $lines[0] = str_replace("\xEF\xBB\xBF", '', $lines[0]);
-
-        $csv = array_map(function($d) use ($delimiter) {
-            # Encode CSV data as UTF-8 (if necessary)
-            if (Butler::encoding($d) != 'UTF-8') {
-                $d = utf8_encode($d);
-            }
-
-            return str_getcsv($d, $delimiter);
-        }, $lines);
-
-        array_walk($csv, function(&$a) use ($csv) {
-            $a = array_combine($csv[0], $a);
-        });
-
-        array_shift($csv);
-
-        return $csv;
-    }
-
-
-    /**
-     * Turns data from a single CSV file (exported from pcbis.de) into a PHP array
-     *
-     * @param string $file - Source CSV file to read data from
      * @param array $headers - Header names for CSV data rows
      * @param string $delimiter - Delimiting character
      * @return array
@@ -75,7 +41,30 @@ class Spreadsheets
         # If no headers available ..
         if (empty($headers)) {
             # .. determine them & return file contents
-            return static::_csvOpen($file, $delimiter);
+            # (1) Load file contents
+            $lines = file($file);
+
+            # (2) Determine header row
+            $lines[0] = str_replace("\xEF\xBB\xBF", '', $lines[0]);
+
+            # (3) Extract CSV data
+            $csv = array_map(function($d) use ($delimiter) {
+                # Encode CSV data as UTF-8 (if necessary)
+                if (Butler::encoding($d) != 'UTF-8') {
+                    $d = utf8_encode($d);
+                }
+
+                return str_getcsv($d, $delimiter);
+            }, $lines);
+
+            # (4) Add header names
+            array_walk($csv, function(&$a) use ($csv) {
+                $a = array_combine($csv[0], $a);
+            });
+
+            array_shift($csv);
+
+            return $csv;
         }
 
         $data = [];
@@ -104,12 +93,13 @@ class Spreadsheets
     /**
      * Extracts data from a single CSV file & processes it
      *
+     * This only works with files exported from `pcbis.de`
+     *
      * @param string $file - Source CSV file to read data from
      * @param string $delimiter - Delimiting character
-     * @param array $headers - Header names for CSV data rows
      * @return array
      */
-    public static function csv2array(string $file, string $delimiter = ';', ?array $headers = null): array
+    public static function csv2array(string $file, string $delimiter = ';'): array
     {
         $data = [];
 
@@ -118,7 +108,7 @@ class Spreadsheets
         }
 
         # Define headers as exported via pcbis.de
-        $headers = $headers ?? [
+        $headers = [
             'AutorIn',
             'Titel',
             'Verlag',
